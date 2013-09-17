f26913fd69bbfb778d81341e303ff7a8a7620e8d
ahmedkaram <ahmedkaramiraq@gmail.com>
2013-09-17 17:20:02 +0300

    update

--- follows.js
+++ follows.js
@@ -12,10 +12,10 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= 'BrnECTdY1jTz0sUbpVp9muUf6H9d5i4gFOqTaWKGPx9cfJ1M', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [1187484229], // Comma separated array of Xively Feed ID numbers
+	var defaultKey		= 'rM6ULnZgggJZ8yJD3KJwKWoZmYDGBq7yPfMUpipDytxX92x6', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [1258549624], // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'Disel Level', // Replaces Xively logo in the header
-		dataDuration	= 'Weeek', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+		dataDuration	= '90days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 10800, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 1; // To hide input form use value of 1, otherwise set to 0
